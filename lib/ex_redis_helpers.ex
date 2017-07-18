7563878772e191c870f5b5054476bb85f250302e
Brian Kereszturi <briankereszturi@gmail.com>
2017-07-18 12:57:52 -0700

    Add rejson helpers.

--- lib/ex_redis_helpers.ex
+++ lib/ex_redis_helpers.ex
@@ -1,9 +1,19 @@
 defmodule ExRedisHelpers do
-  import Exredis.Api
+  alias Exredis, as: E
+  alias Exredis.Api, as: R
 
   defp client(), do: Process.whereis(:redis_client)
 
-  def get(key), do: client() |> get(key)
-  def del(key), do: client() |> del(key)
-  def set(key, value), do: client() |> set(key, value)
+  def get(key), do: client() |> R.get(key)
+  def del(key), do: client() |> R.del(key)
+  def set(key, value), do: client() |> R.set(key, value)
+
+  def json_get(key, path \\ "."),
+    do: client() |> E.query ["JSON.GET", key, path]
+
+  def json_del(key, path \\ "."),
+    do: client() |> E.query ["JSON.DEL", key, path]
+
+  def json_set(key, path, value),
+    do: client() |> E.query ["JSON.SET", key, path, value]
 end
